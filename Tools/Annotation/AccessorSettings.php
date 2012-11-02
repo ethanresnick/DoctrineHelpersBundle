c2dc8cd347e7648a76cb5c640e7aa78d2eb3aa46
Ethan Resnick <ethan.resnick@gmail.com>
2012-11-02 17:25:44 -0400

    Make AccessorSettings annotations inheritable/overrideable in subclasses
    Leverages the InheritableAnnotation from the
    [DoctrineAnnotationsHelpers](https://github.com/ethanresnick/DoctrineAnn
    otationHelpers) library.

--- Tools/Annotation/AccessorSettings.php
+++ Tools/Annotation/AccessorSettings.php
@@ -1,6 +1,6 @@
 <?php
-
 namespace ERD\DoctrineHelpersBundle\Tools\Annotation;
+use ERD\AnnotationHelpers\InheritableAnnotation;
 
 /**
  * Represents an annotation that instructs the AccessorGenerator on the type of accessors to generate.
@@ -10,52 +10,30 @@ namespace ERD\DoctrineHelpersBundle\Tools\Annotation;
  * @copyright Jun 25, 2012 Ethan Resnick Design
  *
  * @Annotation
+ * @property string $visibility The visibility of this property. Can be one of the visibility constants.
+ * @property string|null $publicName The public name of the property, which will be used in the accessors.
+ * If null, the property's name in the class will be used. This value allows you to expose an internal property
+ * under a different method name, which is great for maintaining backwards compatibility or fulfilling an interface.
+ * @property string|null $singular This property's *public name* as a singular. E.g. "page" for a property "pages".
+ * @property string $otherSideMethodEnding For bidirectional many-to-many relationships, we can keep them in sync
+ * automatically, but only if we know how to call the corresponding method (eg. $subject->addX() corresponds to
+ * $x->addSubject()) on the other side. So this variable stores the ending (i.e. method name without add/remove) of
+ * the other side method.
  */
-class AccessorSettings
+class AccessorSettings extends InheritableAnnotation
 {
     const VISIBILITY_PUBLIC = 'public';
     const VISIBILITY_PRIVATE = 'private';
     const VISIBILITY_READONLY = 'read-only';
     const VISIBILITY_WRITEONLY = 'write-only';
 
-    /**
-     * @var string The visibility of this property. Can be one of the visibility constants.
-     */
-    public $visibility = self::VISIBILITY_PUBLIC;
-
-    /**
-     * @var string|null The public name of the property, which will be used in the accessors.
-     * If null, the property's name in the class will be used. This value allows you to
-     * expose an internal property under a different method name, which is great for maintaining
-     * backwards compatibility or fulfilling an interface.
-     */
-    public $publicName = null;
-
-    /**
-     * @var string|null This property's public name as a singular. E.g. "page" for a property "pages".
-     * Because the singular is only used in the names of add/remove methods for collections, whereas
-     * inside the method the collection's plural name is used, it's always the singular of the *public*
-     * name (if the private and public names differ).
-     */
-    public $singular = null;
-
-    /**
-     * @var string For bidirectional many-to-many relationships, we can keep them in sync
-     * automatically, but only if we know how to call the corresponding method (eg.
-     * $subject->addX() corresponds to $x->addSubject()) on the other side. So this variable
-     * stores the ending (i.e. method name without add/remove) of the other side method.
-     */
-    public $otherSideMethodEnding = null;
-
-
     public function __construct(array $data)
     {
-        if(isset($data['value'])) { $this->visibility  = $data['value']; }
+        $this->addAllowedProperties(['visibility','publicName','singular','otherSideMethodEnding']);
+
+        if(isset($data['value']) && !isset($data['visibility'])) { $this->visibility  = $data['value']; }
         if(isset($data['otherSideMethod'])) { $this->otherSideMethodEnding = $data['otherSideMethod']; }
 
-        foreach(['publicName','singular'] as $allowedKey)
-        {
-            if(isset($data[$allowedKey])) { $this->{$allowedKey} = $data[$allowedKey]; }
-        }
+        parent::__construct($data);
     }
 }
